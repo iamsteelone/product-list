de17f10698ff67a46d3493bf6d75b6d0e8dd23d0
Paul Bailey <pbailey@grademaker.com>
2024-08-28 13:56:27 +0100

    i stole their css - wasnt gonna write my own

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React from 'react';
 
-function SearchBar() {}
+const PRODUCE = [
+  {category: "Fruits", price: "£1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "£1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "£2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "£2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "£4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "£1", stocked: true, name: "Peas"}
+];
 
-function ProductTable() {}
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+function ProductTable() {
+  return (
+    <>
+      <table>
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+          </thead>
+        <tbody>
+
+        </tbody>
+      </table>
+    </>
+  )
+}
 
 function ProductCategory() {}
 
